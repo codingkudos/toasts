56871ae051579d39cae62e7eda19e4fd99f8a60d
codingkudos <75491359+codingkudos@users.noreply.github.com>
2020-12-04 15:36:26 +0000

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.codingkudos.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonToast;
+    Context context = MainActivity.this;
+    String message = "You clicked the button";
+    int duration = Toast.LENGTH_LONG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonToast = findViewById(R.id.buttonToast);
+        buttonToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "You clicked the button", Toast.LENGTH_LONG).show();
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+        });
+    }
+}
